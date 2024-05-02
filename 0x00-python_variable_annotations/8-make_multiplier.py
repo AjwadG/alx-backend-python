33e8106ec731803eba644eeb5b974bb22f86b54e
AjwadG <ajwadelgandouz@yahoo.com>
2024-05-02 17:06:53 +0200

    8. Complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+8-make_multiplier.py
+'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    ''' returns a Tuble of (k, v^2) '''
+    return lambda n: n * multiplier
