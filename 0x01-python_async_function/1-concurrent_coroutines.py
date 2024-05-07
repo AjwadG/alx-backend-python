3d13e4b0565c56a2636ef5b2aea4456c983895bc
AjwadG <ajwadelgandouz@yahoo.com>
2024-05-07 02:35:48 +0200

    2. Measure the runtime and doc for n.1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,9 +9,26 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''
-        return a list of float of runing
-        wait_ranomd n number of times
-    '''
+    """
+    Return a list of floats of running wait_random n number of times
+
+    Parameters
+    ----------
+    n: int
+        number of times to run wait_random
+    max_delay: int
+        maximum delay each function call should have
+
+    Returns
+    -------
+    List[float]
+        a list of floats of the running wait_random functions
+    """
     output: List[float] = [wait_random(max_delay) for a in range(n)]
-    return [await func for func in asyncio.as_completed(output)]
+    # use as_completed to run all functions at once
+    return [
+        # use await to get the result of the function
+        await func
+        # for function in the list of functions returned from as_completed
+        for func in asyncio.as_completed(output)
+    ]
