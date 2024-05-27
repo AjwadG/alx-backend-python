37a24a7db7501d033e3e082b7326a46928e51537
AjwadG <ajwadelgandouz@yahoo.com>
2024-05-27 20:50:29 +0200

    7. Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -50,6 +50,15 @@ class TestGithubOrgClient(unittest.TestCase):
             org = GithubOrgClient("google")
             self.assertEqual(org._public_repos_url, repo)
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        ''' Test has license '''
+        output = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(output, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
